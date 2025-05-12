610ce27e9c1c780440991bbb33c0e3338529f783
user <user@Sashas-MacBook-Pro.local>
2025-05-12 17:27:45 +0300

    update webhook

--- bot.py
+++ bot.py
@@ -14,30 +14,34 @@ from config import BOT_TOKEN, WEBHOOK_SECRET
 from handlers import start, button_callback
 
 
-# Flask-приложение
 app = Flask(__name__)
 
-# Создаём Telegram-приложение
 application = ApplicationBuilder().token(BOT_TOKEN).build()
 
-# Подключаем хэндлеры
 application.add_handler(CommandHandler("start", start))
 application.add_handler(CallbackQueryHandler(button_callback))
 
 
-# Вебхукаем!
 @app.post(f"/webhook/{WEBHOOK_SECRET}")
 async def handle_webhook():
     update = Update.de_json(request.get_json(force=True), application.bot)
     await application.process_update(update)
     return "ok", 200
 
-
-def main():
+async def setup():
+    await application.initialize()
     webhook_url = f"https://{os.getenv('RENDER_EXTERNAL_HOSTNAME')}/webhook/{WEBHOOK_SECRET}"
-    asyncio.run(application.bot.set_webhook(webhook_url))
+    await application.bot.set_webhook(webhook_url)
     print(f"Webhook set to: {webhook_url}")
 
+
+def main():
+    # webhook_url = f"https://{os.getenv('RENDER_EXTERNAL_HOSTNAME')}/webhook/{WEBHOOK_SECRET}"
+    # asyncio.run(application.initialize())
+    # asyncio.run(application.bot.set_webhook(webhook_url))
+    # print(f"Webhook set to: {webhook_url}")
+    
+    asyncio.run(setup())
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
 
